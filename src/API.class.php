03ccc99afcd15c4f3dbee1c5536a70dee729763d
Matthias Schneider <ms@flurp.de>
2019-08-04 15:30:42 +0200

    added apcu caching

--- src/API.class.php
+++ src/API.class.php
@@ -3,31 +3,44 @@ namespace TTNUlm;
 
 class API {
 
+    // TODO: write Comm class
     public function returnData($id, $from, $to) {
 
-        $res = DB::create()->getData($id, $from, $to);
+        $cachedResult = apcu_fetch('data');
+        if ($cachedResult !== false) {
 
-        $clean = array_map(function($a){
-            //Example: 2019-06-19T22:04:57.579645412Z
-            $date = \DateTime::createFromFormat('Y-m-d\TH:i:s+', $a['time']);
-            return [
-                'time' => $date->format('Y-m-d H:i:s'),
-                'shtTemp' => $a['payload_fields_shtTemp'],
-                'shtHum' => $a['payload_fields_shtHum'],
-                'bmpTemp' => $a['payload_fields_bmpTemp'],
-                'bmpPres' => $a['payload_fields_bmpPres'],
-                'lux' => $a['payload_fields_lux'],
-                'uva' => $a['payload_fields_uva'],
-                'uvb' => $a['payload_fields_uvb'],
-                'uvi' => $a['payload_fields_uvi'],
-                'battery' => $a['payload_fields_battery'],
-            ];
-        }, $res);
+            echo $cachedResult;
 
-        if ($clean) {
-            echo json_encode($clean);
         } else {
-            echo json_encode([]);
+
+            $res = DB::create()->getData($id, $from, $to);
+
+            $clean = array_map(function ($a) {
+                //Example: 2019-06-19T22:04:57.579645412Z
+                $date = \DateTime::createFromFormat('Y-m-d\TH:i:s+', $a['time']);
+                return [
+                    'time' => $date->format('Y-m-d H:i:s'),
+                    'shtTemp' => $a['payload_fields_shtTemp'],
+                    'shtHum' => $a['payload_fields_shtHum'],
+                    'bmpTemp' => $a['payload_fields_bmpTemp'],
+                    'bmpPres' => $a['payload_fields_bmpPres'],
+                    'lux' => $a['payload_fields_lux'],
+                    'uva' => $a['payload_fields_uva'],
+                    'uvb' => $a['payload_fields_uvb'],
+                    'uvi' => $a['payload_fields_uvi'],
+                    'battery' => $a['payload_fields_battery'],
+                ];
+            }, $res);
+
+            // save cached result
+            apcu_store('data', $clean, 5); // for 5s
+
+            if ($clean) {
+                echo json_encode($clean);
+            } else {
+                echo json_encode([]);
+            }
+
         }
     }
 
